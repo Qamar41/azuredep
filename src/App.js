09d8e85c4c7d186ba5f39320b2af41045b0370fd
Qamar <qamarashfaq41@gmail.com>
2021-06-25 13:23:43 +0500

    final

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello Qamar</h1>
+    <p> now the code is up </p>
+    </div>
+  );
+}
+
+export default App;
